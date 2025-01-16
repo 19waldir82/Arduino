6b1f26c592d2b59a561be7a56a93e7778c63b36a
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2025-01-16 20:07:37 -0300

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,20 @@
+int led = 1;
+
+
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+
+}
+
+
+
+void loop() {
+
+  digitalWrite(led, HIGH);  
+  delay(2000);                
+  digitalWrite(led, LOW);   
+  delay(2000);     
+                                 
+}
